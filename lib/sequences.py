2c564c5d91b5f5e97cf567939ae0d29e631c3938
Grant Cummings <grantcummings4429@gmail.com>
2024-05-13 16:26:40 -0400

    finished lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return print([])
+    elif length == 1:
+        return print([0])
+    fib_sequence = [0, 1]  # Initialize Fibonacci sequence with first two numbers
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]  # Calculate the next Fibonacci number
+        fib_sequence.append(next_num)  # Add the next number to the sequence
+    print(fib_sequence)
+
+
+print_fibonacci(9)
